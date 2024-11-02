96b0d86b2a9c3b2b6aa1098181c7c5529ff5ddfd
MohanManirul <fiforeg@gmail.com>
2024-11-02 22:47:35 +0600

    fopen

---
+++ batch5/module9/mycode/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$filename = "/laragon/www/support/batch5/module9/mycode/f1.txt";
+$fp = fopen($filename,'r');
+$line = fgets($fp);
+echo $line;
+$line = fgets($fp);
+echo $line;
+$line = fgets($fp);
+echo $line;
+$line = fgets($fp);
+echo $line;
+$line = fgets($fp);
+echo $line;
+
+  
+
+
+
+?>
