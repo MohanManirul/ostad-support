12e2326c64b5f976808e7d38fe3c5ff06b351b83
MohanManirul <fiforeg@gmail.com>
2024-11-03 17:27:54 +0600

    file_get_contents

--- batch5/module9/mycode/index.php
+++ batch5/module9/mycode/index.php
@@ -7,15 +7,19 @@ while($line = fgets($fp)){
 }
 
 rewind($fp);
+
 fseek($fp,-1,SEEK_END);
+
 fseek($fp,-1,SEEK_END);
 
 while($line = fgets($fp,5)){
     echo $line."-" ;
 }
-  
-$data = file($filename);
+
+$data = file($filename); // print as array
+$data = file_get_contents($filename);
 print_r($data);
+// echo $data ;
 
 
 
