734503d12e3076185c89c09508c7d8cebabbf02a
MohanManirul <fiforeg@gmail.com>
2024-11-23 13:50:25 +0600

    alpine js data viewd

--- batch4/module22/alpineJs/resources/views/alpine/demo.blade.php
+++ batch4/module22/alpineJs/resources/views/alpine/demo.blade.php
@@ -1,3 +1,32 @@
-<div>
-    <!-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/js/app.js'])
+    <link rel="stylesheet" href="https://matcha.mizu.sh/matcha.css">
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
+</head>
+<body>
+    <p>{{ $title }}</p>
+    <p>demo with alpine js</p>
+
+    <div x-data="data">
+        <p x-text="message"></p>
+        <p x-text="date"></p>
+    </div>
+
+    <script>
+       
+        const data = {
+            
+            message: 'Hello from Alpine.js',
+            date: '15 nov 2024',
+           
+                 
+        }
+    </script>
+</body>
+</html>
