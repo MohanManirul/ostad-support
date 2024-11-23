e5a3a451493168a5c5ca2b57c221f469fb0ff736
MohanManirul <fiforeg@gmail.com>
2024-11-23 23:32:01 +0600

    toLocaleTimeString()

--- batch4/module23/myCode/basic/src/App.vue
+++ batch4/module23/myCode/basic/src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
-
+  const message = "hello"
+  const dara = "hello"
+  function getData(){
+    const data = new Date();
+    return data.toLocaleTimeString();
+  }
 </script>
 
 <template>
-
+  <h2>Hello</h2>
+  <h2>{{getData()}}</h2>
 </template>
 
-<style >
+<style scoped>
 
 </style>
