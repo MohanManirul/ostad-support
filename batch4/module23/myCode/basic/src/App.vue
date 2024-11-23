9df46e6434493a5bb35a34e9128c670d6c9b431e
MohanManirul <fiforeg@gmail.com>
2024-11-23 23:39:00 +0600

    array

--- batch4/module23/myCode/basic/src/App.vue
+++ batch4/module23/myCode/basic/src/App.vue
@@ -2,13 +2,15 @@
   
   const  getData = {
       firstName : "kushiara",
-      lastName : "Nodi"
+      lastName : "Nodi",
+      tasks : [1,2,3,4,5,6,7,8,9]
   }
 </script>
 
 <template>
   <h2>Hello</h2>
   <h2>{{getData.firstName}} - {{getData.lastName}}</h2>
+  <h2>{{getData.tasks[1]}}</h2>
 </template>
 
 <style scoped>
