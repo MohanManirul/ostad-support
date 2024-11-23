dcdc581eac3518e1649b6379482f3669e587b26e
MohanManirul <fiforeg@gmail.com>
2024-11-24 00:15:09 +0600

    image show in vue

--- batch4/module23/myCode/basic/src/App.vue
+++ batch4/module23/myCode/basic/src/App.vue
@@ -2,12 +2,35 @@
   
 import {getData} from "./data/data.js";
 
+const images = [
+      {
+        image :"https://ecologytraining.co.uk/wp-content/uploads/2021/07/Birds-for-Beginners-450x336.jpg",
+        'title':"dird one",
+        'descriptions':"https://ecologytraining.co.uk/wp-content/uploads/2021/07/Birds-for-Beginners-450x336.jpg"
+      },
+      {
+        image :"https://ecologytraining.co.uk/wp-content/uploads/2021/07/Birds-for-Beginners-450x336.jpg",
+        'title':"dird one",
+        'descriptions':"https://ecologytraining.co.uk/wp-content/uploads/2021/07/Birds-for-Beginners-450x336.jpg"
+      },
+      {
+        image :"https://ecologytraining.co.uk/wp-content/uploads/2021/07/Birds-for-Beginners-450x336.jpg",
+        'title':"dird one",
+        'descriptions':"https://ecologytraining.co.uk/wp-content/uploads/2021/07/Birds-for-Beginners-450x336.jpg"
+      }
+
+  ];
+
+
 </script>
 
 <template>
   <h2>Hello</h2>
   <h2>{{getData.firstName}} - {{getData.lastName}}</h2>
   <h2>{{getData.tasks[1]}}</h2>
+  <div>
+    <img v-bind:title="images[0].title" src="https://ecologytraining.co.uk/wp-content/uploads/2021/07/Birds-for-Beginners-450x336.jpg">
+  </div>
 </template>
 
 <style scoped>
