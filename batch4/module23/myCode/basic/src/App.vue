20931e5a545bda7c00cf9a38706bcb5015240264
MohanManirul <fiforeg@gmail.com>
2024-11-23 23:43:01 +0600

    get data from external source

--- batch4/module23/myCode/basic/src/App.vue
+++ batch4/module23/myCode/basic/src/App.vue
@@ -1,10 +1,7 @@
 <script setup>
   
-  const  getData = {
-      firstName : "kushiara",
-      lastName : "Nodi",
-      tasks : [1,2,3,4,5,6,7,8,9]
-  }
+import {getData} from "./data/data.js";
+
 </script>
 
 <template>
