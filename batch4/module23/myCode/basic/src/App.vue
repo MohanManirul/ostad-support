089d816b5d85ec6801af41dd9bf532cbc2448f04
MohanManirul <fiforeg@gmail.com>
2024-11-23 23:35:39 +0600

    object

--- batch4/module23/myCode/basic/src/App.vue
+++ batch4/module23/myCode/basic/src/App.vue
@@ -1,15 +1,14 @@
 <script setup>
-  const message = "hello"
-  const dara = "hello"
-  function getData(){
-    const data = new Date();
-    return data.toLocaleTimeString();
+  
+  const  getData = {
+      firstName : "kushiara",
+      lastName : "Nodi"
   }
 </script>
 
 <template>
   <h2>Hello</h2>
-  <h2>{{getData()}}</h2>
+  <h2>{{getData.firstName}} - {{getData.lastName}}</h2>
 </template>
 
 <style scoped>
